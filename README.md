4709d36babedc0f2330a95b10fdaecb2186043b6
VladimirZag <zagorodnyukvolod@gmail.com>
2023-04-09 14:41:54 +0300

    added Group activity

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TelegramNotificationBot
 Telegram bot that makes notifications for your group using Spring.
+https://t.me/Notifications1XklmrBots1_bot
--- TelegramNotificationBot/src/main/Java/BotConfig/BotService.java
+++ TelegramNotificationBot/src/main/Java/BotConfig/BotService.java
@@ -2,19 +2,14 @@ package BotConfig;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.CallbackQuery;
-import com.pengrad.telegrambot.model.InlineQuery;
-import com.pengrad.telegrambot.model.Message;
-import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.*;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.model.request.InlineQueryResultArticle;
 import com.pengrad.telegrambot.request.AnswerInlineQuery;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
 
 public class BotService {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BotConfig.class);
@@ -43,25 +38,28 @@ public class BotService {
             InlineQueryResultArticle inlineDelete =
                     new InlineQueryResultArticle("delete", "Удалить", " Удалить уведомление");
             InlineQueryResultArticle inlineHelp =
-                    new InlineQueryResultArticle("help", "Помощь", "Справка");
+                    new InlineQueryResultArticle("help", "Помощь", "/start");
             request = new AnswerInlineQuery(inlineQuery.id(), inlineCreate, inlineEdit, inlineDelete, inlineHelp);
+            DoRequest(request);
 
 
         } else if (message != null) {
             long chatId = message.chat().id();
-            
-            request = new SendMessage(chatId, "Hello " + update.message().chat().firstName() + "!");
-            switch (message.text()) {
-                case ("/start"): {
-                    request = new SendMessage(chatId, "Добавьте бот в нужный чат. Вызовите через '@' id бота и выберите нужное действие. Для подробной информации нажмите команду 'Помощь'");
-                    break;
-                }
+            if ("/start".equals(message.text())) {
+                request = new SendMessage(chatId, "Привет, " + message.from().firstName() + "! Добавьте бот в нужный чат и назначьте его администратором. Вызовите через '@' id бота и выберите нужное действие.");
+                DoRequest(request);
             }
         }
+//        if (update.message().chat().joinByRequest() != null && message != null) {
+//            request = new SendMessage(message.chat().id(), "Привет! Введите команду '/start'");
+//            DoRequest(request);
+//        }
 
+    }
+
+    public void DoRequest(BaseRequest request) {
         if (request != null) {
             bot.execute(request);
         }
-
     }
 }
